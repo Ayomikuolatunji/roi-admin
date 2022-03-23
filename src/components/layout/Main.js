093839b8c05939d8afa4b0194463b04a3d1da86c
Ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-03-23 13:47:06 +0100

    adding pie chat

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -5,6 +5,7 @@ import { Pie } from 'react-chartjs-2';
 import {data} from "../../hooks/PIE.jsx"
 
 export default function Main() {
+  console.log(typeof data())
   const [prods,setprods]=React.useState([]);
   const [prodLoading,setProloading]=React.useState(true);
   let abort=React.useRef(true)
@@ -42,55 +43,56 @@ export default function Main() {
 }
   return (
     <div className='sm:p-3 py-2 mx-auto w-11/12'>
-      <div className="pie">
-        <Pie  data={data}/>
+        <div class="grid  lg:grid-cols-2 gap-5 w-full p-4 grid-cols-1">
+          <div class="bg-green-200 py-2 rounded-md ">
+            <div class="flex">
+              <div>
+              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL EXPORTS PODUCTS</h2>
+              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"EXPORT").length}</h2>
+              </div>
+              <div class="flex justify-center items-center">
+              <AiOutlineShopping className=' text-7xl'/>
+              </div>
+            </div>
+          </div>
+          <div class="bg-yellow-200 py-2 rounded-md ">
+            <div class="flex">
+              <div>
+              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL LOCAL PRODUCTS</h2>
+              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"LOCAL").length}</h2>
+              </div>
+              <div class="flex justify-center items-center">
+              <AiOutlineShopping className=' text-7xl'/>
+              </div>
+            </div>
+          </div>
+          <div class="bg-red-200 py-2 rounded-md ">
+            <div class="flex">
+              <div>
+              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL REAL ESTATE PRODUCTS</h2>
+              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"ESTATE").length}</h2>
+              </div>
+              <div class="flex justify-center items-center">
+              <AiOutlineShopping className=' text-7xl'/>
+              </div>
+            </div>
+          </div>
+          <div class="bg-blue-200 py-2 rounded-md ">
+            <div class="flex">
+              <div>
+              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL MINERAL RESOURCES PRODUCTS</h2>
+              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"MINERAL").length}</h2>
+              </div>
+              <div class="flex justify-center items-center">
+                <AiOutlineShopping className=' text-7xl'/>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="pie sm:w-1/2 mx-auto mt-12">
+          <h1 className='text-center text-yellow-500 font-extrabold text-4xl'>Product Pie Chats</h1>
+        <Pie  data={data(newProd(prods,"EXPORT").length, newProd(prods,"LOCAL").length,newProd(prods,"ESTATE").length,newProd(prods,"MINERAL").length,newProd(prods,"LOCAL").length)}/>
       </div>
-<div class="grid  lg:grid-cols-2 gap-5 w-full p-4 grid-cols-1">
-  <div class="bg-green-200 py-2 rounded-md ">
-    <div class="flex">
-      <div>
-      <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL EXPORTS PODUCTS</h2>
-      <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"EXPORT").length}</h2>
-      </div>
-      <div class="flex justify-center items-center">
-       <AiOutlineShopping className=' text-7xl'/>
-      </div>
-    </div>
-  </div>
-  <div class="bg-yellow-200 py-2 rounded-md ">
-    <div class="flex">
-      <div>
-      <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL LOCAL PRODUCTS</h2>
-      <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"LOCAL").length}</h2>
-      </div>
-      <div class="flex justify-center items-center">
-      <AiOutlineShopping className=' text-7xl'/>
-      </div>
-    </div>
-  </div>
-  <div class="bg-red-200 py-2 rounded-md ">
-    <div class="flex">
-      <div>
-      <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL REAL ESTATE PRODUCTS</h2>
-      <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"ESTATE").length}</h2>
-      </div>
-      <div class="flex justify-center items-center">
-      <AiOutlineShopping className=' text-7xl'/>
-      </div>
-    </div>
-  </div>
-  <div class="bg-blue-200 py-2 rounded-md ">
-    <div class="flex">
-      <div>
-      <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL MINERAL RESOURCES PRODUCTS</h2>
-      <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"MINERAL").length}</h2>
-      </div>
-      <div class="flex justify-center items-center">
-         <AiOutlineShopping className=' text-7xl'/>
-      </div>
-    </div>
-  </div>
-</div>
     </div>
   )
 }
--- src/hooks/PIE.jsx
+++ src/hooks/PIE.jsx
@@ -4,12 +4,14 @@ import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export const data = {
-  labels: ['EXPORT', 'LOCAL', 'REAL ESTATE', 'MINERAL RESOURCES'],
+export const data =(es,lo,re,mi)=>{
+  
+  return {
+    labels: ['EXPORT', 'LOCAL', 'REAL ESTATE', 'MINERAL RESOURCES'],
   datasets: [
     {
       label: '# of Votes',
-      data: [12, 19, 3, 5],
+      data: [es, lo, re, mi],
       backgroundColor: [
         'rgba(255, 99, 132, 0.2)',
         'rgba(54, 162, 235, 0.2)',
@@ -22,9 +24,10 @@ export const data = {
         'rgba(255, 206, 86, 1)',
         'rgba(75, 192, 192, 1)',
       ],
-      with:"100px",
       maxWidth: "100%",
       borderWidth: 1,
     },
   ],
+  }
+  
 };
