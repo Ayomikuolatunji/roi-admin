1b7f697f4b2283adf70e21f99d09480b0e6857f2
Ayomikuolatunji <ayomikuolatunji@gmail.com>
2022-03-23 13:51:26 +0100

    adding pie chat

--- src/components/layout/Main.js
+++ src/components/layout/Main.js
@@ -5,7 +5,6 @@ import { Pie } from 'react-chartjs-2';
 import {data} from "../../hooks/PIE.jsx"
 
 export default function Main() {
-  console.log(typeof data())
   const [prods,setprods]=React.useState([]);
   const [prodLoading,setProloading]=React.useState(true);
   let abort=React.useRef(true)
@@ -43,47 +42,47 @@ export default function Main() {
 }
   return (
     <div className='sm:p-3 py-2 mx-auto w-11/12'>
-        <div class="grid  lg:grid-cols-2 gap-5 w-full p-4 grid-cols-1">
-          <div class="bg-green-200 py-2 rounded-md ">
-            <div class="flex">
+        <div className="grid  lg:grid-cols-2 gap-5 w-full p-4 grid-cols-1">
+          <div className="bg-red-100 py-2 rounded-md ">
+            <div className="flex">
               <div>
-              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL EXPORTS PODUCTS</h2>
-              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"EXPORT").length}</h2>
+              <h2 className="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL EXPORTS PODUCTS</h2>
+              <h2 className="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"EXPORT").length}</h2>
               </div>
-              <div class="flex justify-center items-center">
+              <div className="flex justify-center items-center">
               <AiOutlineShopping className=' text-7xl'/>
               </div>
             </div>
           </div>
-          <div class="bg-yellow-200 py-2 rounded-md ">
-            <div class="flex">
+          <div className="bg-blue-100 py-2 rounded-md ">
+            <div className="flex">
               <div>
-              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL LOCAL PRODUCTS</h2>
-              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"LOCAL").length}</h2>
+              <h2 className="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL LOCAL PRODUCTS</h2>
+              <h2 className="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"LOCAL").length}</h2>
               </div>
-              <div class="flex justify-center items-center">
+              <div className="flex justify-center items-center">
               <AiOutlineShopping className=' text-7xl'/>
               </div>
             </div>
           </div>
-          <div class="bg-red-200 py-2 rounded-md ">
-            <div class="flex">
+          <div className="bg-yellow-100 py-2 rounded-md ">
+            <div className="flex">
               <div>
-              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL REAL ESTATE PRODUCTS</h2>
-              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"ESTATE").length}</h2>
+              <h2 className="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL REAL ESTATE PRODUCTS</h2>
+              <h2 className="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"ESTATE").length}</h2>
               </div>
-              <div class="flex justify-center items-center">
+              <div className="flex justify-center items-center">
               <AiOutlineShopping className=' text-7xl'/>
               </div>
             </div>
           </div>
-          <div class="bg-blue-200 py-2 rounded-md ">
-            <div class="flex">
+          <div className="bg-green-100 py-2 rounded-md ">
+            <div className="flex">
               <div>
-              <h2 class="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL MINERAL RESOURCES PRODUCTS</h2>
-              <h2 class="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"MINERAL").length}</h2>
+              <h2 className="text-base md:text-2xl lg:text-5xl px-2 whitespace-no-wrap text-gray-600">TOTAL MINERAL RESOURCES PRODUCTS</h2>
+              <h2 className="ltext-base md:text-2xl lg:text-5xl px-4 py-2 lg:px-8 text-gray-600">{newProd(prods,"MINERAL").length}</h2>
               </div>
-              <div class="flex justify-center items-center">
+              <div className="flex justify-center items-center">
                 <AiOutlineShopping className=' text-7xl'/>
               </div>
             </div>
